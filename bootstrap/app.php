c3c0e4b6280405049afc80beade494ad25950d4e
Kresna Satya <kresnasatya72@gmail.com>
2025-03-23 06:54:57 +0800

    feat: add sample jobs + scheduling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Jobs\LogCatcherJob;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,4 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            LogCatcherJob::dispatch();
+        })
+        ->name('Log Catcher Job')
+        ->everyFiveSeconds();
+    })
+    ->create();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Jobs\HelloJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello-job', function () {
+    HelloJob::dispatch();
+    return 'Finished';
 });
